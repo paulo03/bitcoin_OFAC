9c6f7af19fab0f7468bff85c84e91aa0e37b8891
oluap <paulo@betopenly.com>
2019-08-21 21:16:16 -0700

    Add files via upload

---
+++ check_SDN_BTC_address_balance.py
@@ -0,0 +1,25 @@
+import SDN_List_Scrape_for_BTC_address
+import pandas as pd
+import datetime
+import requests
+from bitcoin_explorer import Address
+
+date = str(datetime.date.today())
+csv = 'OFAC_BTC_ADDRESS_CHECK_' + date + '.csv'
+
+df = pd.read_csv(csv)
+OFAC_coin_balances = []
+
+df_btc = df[df['Coin'] == 'XBT']
+
+for item in df_btc['Address']:
+    a = Address(item)
+    OFAC_coin_balances.append([a.address, a.balance, a.pending])
+
+df_ofac = pd.DataFrame(OFAC_coin_balances,
+                       columns=['Address', 'Balance', 'Unconfirmed Coin'])
+
+# Export csv with BTC addresses on OFAC with balances and pending coins
+csv_BTC_OFAC = 'OFAC_BTC_ADDRESS_WITH_BALANCE' + date + '.csv'
+export_csv = df_ofac.to_csv(csv_BTC_OFAC, index=False)
+
